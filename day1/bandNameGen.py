f6f57a6f7e2b43bf8ebab28552c0445709416fc6
bbjarvis <brettbjarvis@gmail.com>
2022-04-08 14:06:04 -0700

    add bandNameGen and printing

---
+++ day1/bandNameGen.py
@@ -0,0 +1,6 @@
+# Write your code below this line 👇
+print('Welcome to the Band Name Generator.')
+city = input("""What's the name of the city that you grew up in?\n""")
+pet = input("""What's your pet's name?\n""")
+
+print("""Your bane name could be""", city, pet, sep=" ")
