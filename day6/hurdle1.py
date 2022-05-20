639442819623420b41ffd198a85876072f0274ac
bbjarvis <brettbjarvis@gmail.com>
2022-05-20 16:51:25 -0700

    add day6, hurdle1.py, Reeborg's World

---
+++ day6/hurdle1.py
@@ -0,0 +1,26 @@
+# Hurdles race
+# Reeborg has entered a hurdles race. Make him run the course, following the path shown.
+# What you need to know
+# The functions move() and turn_left().
+# Difficulty level
+
+# More advanced
+# You may have noticed that your solution has some repeated patterns. 
+# If you know how to define functions, define a function named jump() and use it to simplify your program.
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+for n in range(6):
+    jump()
